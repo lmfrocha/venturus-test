cf7ee36f6ec2c5b308c94165068f2e474ad3d492
Lucas Rocha <lucas.marcelino@outlook.com>
2026-04-16 01:59:11 -0300

    feat: upgrade versions

--- venturus-hacker-hanck-operadores/src/test/java/br/com/venturus/api/operand/OperandTest.java
+++ venturus-hacker-hanck-operadores/src/test/java/br/com/venturus/api/operand/OperandTest.java
@@ -6,11 +6,8 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class OperandTest {
 
